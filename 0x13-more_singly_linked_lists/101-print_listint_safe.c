adac556de9a847cd29342b86004160d086fc0f00
es-sebbani <69191683+es-sebbani@users.noreply.github.com>
2023-08-28 16:40:07 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,70 +1,31 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints all the elements of a linked list
- * @head: head of the list
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node required
  *
- * Return: the number of nodes
+ * Return: the address of the node
  */
-size_t print_listint_safe(const listint_t *head)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	const listint_t *cursor = head;
-	listint_t **ptrs;
-	unsigned int list_len = listint_len(head);
-	size_t count = 0;
+	listint_t *cursor = NULL;
+	unsigned int i = 0;
 
-	ptrs = malloc(sizeof(listint_t) * list_len);
-	if (ptrs == NULL)
-		exit(98);
-	while (cursor == 0)
+	while (head != NULL)
 	{
-		if (check_ptr(cursor, ptrs, list_len) == 0)
+		if (i <= index)
 		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
+			if (i == index)
+			{
+				cursor = head;
+				break;
+			}
+			head = head->next;
+			i++;
 		}
 		else
-		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
-		}
-		count += 1;
-		cursor = cursor->next;
+			return (NULL);
 	}
-	return (count);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
-}
-
-/**
- * check_ptr - checks if a pointer is in an array
- * @ptr: pointer to be checked
- * @array: array to be checked in
- * @size: size of the array
- *
- * Return: 1 on success, 0 on fail
- */
-int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
-{
-	while (size-- >= 0)
-	{
-		if (ptr == array[size])
-			return (1);
-	}
-	return (0);
+	return (cursor);
 }
